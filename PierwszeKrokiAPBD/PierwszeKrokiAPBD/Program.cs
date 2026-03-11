e746d655b388e5ba03f71782f7b1df96bff60c0e
s34113 <antraks123@gmail.com>
2026-03-11 13:11:58 +0100

    dokończenie CalculateMin

--- PierwszeKrokiAPBD/PierwszeKrokiAPBD/Program.cs
+++ PierwszeKrokiAPBD/PierwszeKrokiAPBD/Program.cs
@@ -33,7 +33,15 @@ Console.WriteLine("ostatnie");
 
 static int CalculateMin(int[] values)
 {
-    return 0;
+    int tmp = values[0];
+    foreach (int value in values)
+    {
+        if (value < tmp)
+        {
+            tmp = value;
+        }
+    }
+        
 }
 
 
