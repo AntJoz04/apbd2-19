dba624ebc89d07afbbf80472246da1a492d83ae1
s34113 <antraks123@gmail.com>
2026-03-11 12:52:03 +0100

    czyszczenie przed zadaniem 3

--- PierwszeKrokiAPBD/PierwszeKrokiAPBD/Program.cs
+++ PierwszeKrokiAPBD/PierwszeKrokiAPBD/Program.cs
@@ -1,9 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Coś tam zmieniam");
-Console.WriteLine("Feature");
-Console.WriteLine("Feature2");
 
 
-Console.WriteLine("A teraz zmieniam w mainie");
