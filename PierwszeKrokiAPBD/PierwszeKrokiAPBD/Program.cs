8b9d2920ccb1fb39b408dd2d6e2e9b38a18c2889
s34113 <antraks123@gmail.com>
2026-03-11 12:42:46 +0100

    zmiany w main

--- PierwszeKrokiAPBD/PierwszeKrokiAPBD/Program.cs
+++ PierwszeKrokiAPBD/PierwszeKrokiAPBD/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Coś tam zmieniam");
+
+
+Console.WriteLine("A teraz zmieniam w mainie");
